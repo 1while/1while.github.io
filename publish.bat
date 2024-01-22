cd17148c303982e6a78b38919a2585763cf95d60
zhou- <hzhouw@foxmail.com>
2024-01-22 17:32:28 +0800

    clean and publish

--- publish.bat
+++ publish.bat
@@ -1,13 +1,23 @@
 
-cd build
 
 @echo off
 cd build
-for /f "delims=" %%i in ('dir /b /a-d ^| findstr /v /b /c:".git"') do (
-    del /q "%%i"
+rmdir /s /q doctrees
+
+cd html
+
+for /f "delims=" %%F in ('dir /b /a-d ^| findstr /v /i /c:".git"') do (
+    echo Deleting %%F...
+    del /f /q "%%F"
 )
 
+for /d %%D in (*) do (
+    if not "%%~nxD" == ".git" (
+        echo Deleting directory %%D...
+        rmdir /s /q "%%D"
+    )
+)
 
 cd %~dp0
 
-@REM call make html
+call make html
